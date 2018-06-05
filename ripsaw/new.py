03b3eed354594fac65c8300a5eab56cb7b331bf6
Philip Loguinov <philip.loguinov@onetick.com>
2018-06-04 22:45:05 -0400

    simplify the new script

--- ripsaw/new.py
+++ ripsaw/new.py
@@ -5,24 +5,21 @@
 
 from pathlib import Path
 import shutil
+import sys
+
 
 #----------------------------------------------------------------------------------------------#
 
-template_file = Path(__file__).resolve().parent / 'new_monitor.py'
+if __name__ == "__main__":
 
-def main(target:Path):
-    '''copy template from package location to target location'''
+    ### copy template from package location to target location
+    template_file = Path(__file__).resolve().parent / 'new_monitor.py'
 
+    target = Path(sys.argv[1]) \
+        if len(sys.argv) > 1 \
+        else Path('.') / 'monitor.py'
 
     shutil.copyfile(str(template_file), str(target))
-
-
-#----------------------------------------------------------------------------------------------#
-if __name__ == "__main__":
-    import sys
-    target = Path(sys.argv[1]) if len(sys.argv) > 1 else Path('.')/'monitor.py'
-
-    main(target)
 
 
 #----------------------------------------------------------------------------------------------#
